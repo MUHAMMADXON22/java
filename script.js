468274abdc22dbbcd9fcadd0f7cf7eea68f29653
MUKHAM <muxammadxon994@gmail.com>
2025-03-27 15:29:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// Запрашиваем имя
+let name1 = prompt("Введите имя:");
+
+ //Запрашиваем букву
+let letter = prompt("Введите space id:");
+
+let password = prompt("Введите space password:");
+
+
+let user = {
+
+};
+
+
+console.log(name)
+
+console.log(user)
+console.log(user.city)//
+console.log(user['age'])//
+
+delete user.age;
+console.log(user)
+
+
+let students = {}
+
+students.name = prompt('Укажите имя');
+students.id = prompt('Введите space id:')
+
+console.log(students)
+
+
